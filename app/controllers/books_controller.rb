75219256fef7fc2e80a54190e389bd79308f1833
reeeho-s <91420022+reeeho-s@users.noreply.github.com>
2021-10-12 14:59:30 +0000

    Add 詳細ページ 3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
@@ -12,9 +13,9 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     book.save
-    redirect_to '/books'
+    redirect_to book_path(book.id)
   end
 
   def edit
