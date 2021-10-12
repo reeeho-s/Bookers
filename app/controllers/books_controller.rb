b54e48be799b87dcb8543ab527b7290554784b2b
reeeho-s <91420022+reeeho-s@users.noreply.github.com>
2021-10-12 13:13:51 +0000

    Add 投稿機能　3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,21 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
   end
 
   def edit
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
