65655a21c88f9cb9f28b6540bbf0a44543dcdd0d
reeeho-s <91420022+reeeho-s@users.noreply.github.com>
2021-10-13 12:43:20 +0000

    Update バリデーション 3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,9 +24,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book), notice: 'Book was successfully updated.'
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
