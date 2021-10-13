8856de2710c1d689cfdff402bf1915ad643d358b
reeeho-s <91420022+reeeho-s@users.noreply.github.com>
2021-10-13 05:51:10 +0000

    Add 削除機能 3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -8,14 +9,10 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
-
   def create
     @book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book.save
+    redirect_to book_path(@book)
   end
 
   def edit
@@ -26,6 +23,12 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
